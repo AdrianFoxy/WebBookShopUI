972c4591ef7fab792c35ac6794d96396c756fc47
AdrianFoxy <drugakadrian@gmail.com>
2023-02-20 07:19:52 +0200

    intrefaceTest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './models/book';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'BookShop';
+  books: Book[] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Book[]>>('https://localhost:7274/api/Book/catalog_books?PageSize=50').subscribe({
+      next: response => this.books = response.data, // what to do next
+      error: error => console.log(error), // what to do if error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statment');
+      }
+    })
+  }
 }
---
+++ src/app/models/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  id: number;
+  title: string;
+  imageURL: string;
+  bookSeries: string;
+  price: number;
+  authors: string[];
+}
