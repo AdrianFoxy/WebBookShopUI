85ad5757f776fef17e3b781676dc23c5914f2feb
AdrianFoxy <drugakadrian@gmail.com>
2023-05-26 18:19:40 +0300

    user-profile-update

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
+
+  activeTab: string = 'orders';
+
+  constructor(public accountService: AccountService) {
+
+  }
 
 }
