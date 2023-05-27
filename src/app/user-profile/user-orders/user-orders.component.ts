ed30b4c192c2a6faa1d0ca572915ce31f8beb83f
AdrianFoxy <drugakadrian@gmail.com>
2023-05-28 01:41:12 +0300

    I`m_very_bolnoi_orders_in_user_profile

--- src/app/user-profile/user-orders/user-orders.component.ts
+++ src/app/user-profile/user-orders/user-orders.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Order } from 'src/app/shared/models/order';
+import { ShopParams } from 'src/app/shared/models/shopParams';
+import { UserProfileService } from '../user-profile.service';
 
 @Component({
   selector: 'app-user-orders',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-orders.component.scss']
 })
 export class UserOrdersComponent {
+
+  orders: Order[] = [];
+  orderTotalCount = 0;
+
+  userParam = new ShopParams();
+
+  constructor(private userProfieleService: UserProfileService) {
+  }
+
+  ngOnInit() {
+    this.getOrderForCurrentUser();
+  }
+
+  getOrderForCurrentUser(){
+    this.userProfieleService.getOrdersForCurrentUser(this.userParam).subscribe({
+      next: response => {
+        this.orders = response.data;
+        this.userParam.pageNumber = response.pageIndex;
+        this.userParam.pageSize = response.pageSize;
+        this.orderTotalCount = response.count;
+      },
+      error: error => console.log(error)
+    })
+  }
 
 }
--- src/app/user-profile/user-profile.service.ts
+++ src/app/user-profile/user-profile.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ShopParams } from '../shared/models/shopParams';
+import { Order } from '../shared/models/order';
+import { Pagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
 
-  constructor() { }
+  baseUrl = 'https://localhost:7274/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getOrdersForCurrentUser(UserParams: ShopParams){
+    let params = new HttpParams();
+
+    if (UserParams.pageNumber)
+      params = params.append('pageIndex', UserParams.pageNumber.toString());
+
+    if (UserParams.pageSize)
+      // kostil for diplom  params = params.append('pageSize', UserParams.pageSize.toString());
+      params = params.append('pageSize', UserParams.pageSize.toString());
+
+    return this.http.get<Pagination<Order[]>>(this.baseUrl + 'Order/get-orders-for-current-user', {params});
+  }
+
 }
